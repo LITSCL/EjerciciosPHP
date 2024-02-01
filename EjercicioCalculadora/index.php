af364d1ae234e80be143ee9ee62a5648f0492893
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2024-02-01 00:29:22 -0300

    Ok

--- EjercicioCalculadora/index.php
+++ EjercicioCalculadora/index.php
@@ -26,7 +26,8 @@ if (!empty($_GET["numero1"]) && !empty($_GET["numero2"]) && !empty($_GET["boton"
 <!doctype html>
 <html lang="es">
 <head>
-	<meta charset="UTF-8" />
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
 </head>
 <body>
